d3c21380929cfe82b8d1170a28d8ca8dd23bba90
ossonts <472243+ossonts@users.noreply.github.com>
2021-08-21 09:38:06 +0100

    newName

--- BuildDeploy.sh
+++ BuildDeploy.sh
@@ -8,6 +8,6 @@ docker login ghcr.io -u ossonts
 docker build -t ghcr.io/setap2021/setapdocker .
 
 # TESTING
-# docker run -it  ghcr.io/m30819-2020/setapdocker
+# docker run -it  ghcr.io/setap2021/setapdocker
 # Sent to github
 docker push ghcr.io/setap2021/setapdocker
